66d2288fb01f697229bd2a0c044f4f7258ced544
Kerminanabil <krkrn9457@gmail.com>
2023-03-18 02:30:34 -0700

    tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void){
+	char alphabet[26] ="abcdefghijklmnopqrstuvwxyz";
+		int i;
+for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
